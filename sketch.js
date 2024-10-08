2f9369b5fdf5d82b9b626b864f8e6dcfcde18b26
Melany Segarra <melanyk.segarra@ucuenca.edu.ec>
2024-10-08 11:14:45 -0500

    se creó una pelota que rebota

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+let posX, posY;
+let velX, velY;
+let diametro;
+let radio;
+let fondoCambiable;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  fondoCambiable = color(random(153, 255), random(115), random(191));
+
+  radio = ceil(random(20, 50));
+  diametro = radio * 2;
+  //console.log(diametro);
+
+  posX = random(radio, width - radio);
+  posY = random(radio, height - radio);
+
+  velX = random(-10, 10);
+  velY = random(-10, 10);
+  //velX = 5;
+  //velY = 5;
 }
 
 function draw() {
-  background(220);
+  background(fondoCambiable);
+  fill(63, 3, 89);
+  noStroke();
+
+  posX += velX;
+  posY += velY;
+
+  // Rebota en el extremo derecho de la pantalla
+  if (posX > width - radio || posX < radio) {
+    velX *= -1;
+    fondoCambiable = color(random(153, 255), random(115), random(191));
+  }
+
+  if (posY > height - radio || posY < radio) {
+    velY *= -1;
+    fondoCambiable = color(random(153, 255), random(115), random(191));
+  }
+  circle(posX, posY, diametro);
 }
