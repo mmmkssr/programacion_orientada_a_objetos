ff12374a72e864b011fd1b1d91342a044c21f7c5
Melany Segarra <melanyk.segarra@ucuenca.edu.ec>
2024-10-13 22:47:36 -0500

    updates

--- sketch.js
+++ sketch.js
@@ -1,63 +1,66 @@
-let posX, posY;
-let velX, velY;
-let diametro;
-let radio;
 let fondoCambiable;
-//crear nueva pelota
+let pelotas = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   fondoCambiable = color(random(153, 255), random(115), random(191));
-  //declarar la nueva pelota
 
-  radio = ceil(random(20, 50));
-  diametro = radio * 2;
-  //console.log(diametro);
+  for (let i = 0; i < 100; i++) {
+    let nuevaPelota = new Pelota();
+    pelotas.push(nuevaPelota);
+  }
 
-  posX = random(radio, width - radio);
-  posY = random(radio, height - radio);
-
-  velX = random(-10, 10);
-  velY = random(-10, 10);
-  //velX = 5;
-  //velY = 5;
+  // New = nomenclatura - js ejecuta la función constructor
 }
 
 function draw() {
   background(fondoCambiable);
+  for (let i = 0; i < 100; i++) {
+    pelotas[i].update();
+    pelotas[i].display();
+  }
   // update y display de la nueva pelota
-  fill(63, 3, 89);
-  noStroke();
-
-  posX += velX;
-  posY += velY;
-
-  // Rebota en el extremo derecho de la pantalla
-  if (posX > width - radio || posX < radio) {
-    velX *= -1;
-    fondoCambiable = color(random(153, 255), random(115), random(191));
-  }
-
-  if (posY > height - radio || posY < radio) {
-    velY *= -1;
-    fondoCambiable = color(random(153, 255), random(115), random(191));
-  }
-  circle(posX, posY, diametro);
 }
-
 class Pelota {
   // Plano de instrucciones para crear una pelota
+
   constructor() {
-    //todas las variables y declararlas a la vez
+    // Se ejecuta automáticamente al crear una nueva pelota
+    // Función interna que se ejecuta al momento de crear la clase
+    // this = forma de decir a js que los objetos están dentro del objeto pelota
+
+    // Todas las variables y declararlas a la vez
+    this.radio = ceil(random(20, 30));
+    this.diametro = this.radio * 2;
+    this.posX = random(this.radio, width - this.radio);
+    this.posY = random(this.radio, height - this.radio);
+    this.velX = random(-10, 10);
+    this.velY = random(-10, 10);
+    console.log("estoy viva!");
   }
 
-  // método update
+  // método update - función interna del objeto pelota
 
   update() {
     // sistema de colisión de la pelota
+
+    this.posX += this.velX;
+    this.posY += this.velY;
+
+    if (this.posX > width - this.radio || this.posX < this.radio) {
+      this.velX *= -1;
+    }
+
+    if (this.posY > height - this.radio || this.posY < this.radio) {
+      this.velY *= -1;
+    }
   }
 
   display() {
     // dibujar la pelota
+
+    fill(63, 3, 89);
+    noStroke();
+    circle(this.posX, this.posY, this.diametro);
   }
 }
