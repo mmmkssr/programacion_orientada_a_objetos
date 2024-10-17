7e20c982ec95cdce80104b739902fd4352299dbb
Melany Segarra <melanyk.segarra@ucuenca.edu.ec>
2024-10-17 16:00:11 -0500

    funciones anónimas

--- sketch.js
+++ sketch.js
@@ -18,12 +18,11 @@ function draw() {
    * Esta es una forma de borrar las partículas muertas utilizando
    * un for loop que navega todas las partículas y se pregunta
    * por la variable boleana estaViva.
-   * 
+   *
    * Utiliza la función splice para borrar la partícula específica
    * mediante su indice
    */
 
-  
   for (let i = 0; i < particulas.length; i++) {
     if (!particulas[i].estaViva) {
       //! pregunta lo contrario
@@ -33,6 +32,24 @@ function draw() {
 
     //particulas = particulas.filter((pelota) => pelota.estaViva);
     //código escrito de forma más eficiente
+
+    //Línea que una los círculos
+
+    //noFill();
+    //stroke(252,99,145);
+    //strokeWeight(1);
+
+    //for (let i = 0; i < partículas.length - 1; i ++){
+
+    //if (particula [i + 1] !== undefined){} - otra forma de dibujar las líneas
+
+    //line(
+    //    particulas [i].posX,
+    //    particulas [i].posY,
+    //    particulas [i + 1].posX,
+    //    particulas [i + 1].posY,
+    //);
+    //}
   }
   console.log(particulas.length);
 }
